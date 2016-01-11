7778ca52478eb15b027b853771519db02cac085e
Terry Song <jtsong8@gmail.com>
2016-01-11 07:29:06 -0500

    Retry after first commit failed

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script a the game's level manager
+// It manages the flow of different game scenes
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log("Level load requested for: "+name);
+		// reset breakable brick count
+		Brick.breakable_Count = 0;
+        // name could be any scene in the game
+        Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log ("Quit requested to leave game");
+		// no effect for debug and web build
+		Application.Quit ();
+	}
+	
+	public void LoadNextLevel() {
+		// reset breakable brick count
+		Brick.breakable_Count = 0;
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+	
+	public void BrickDestroyed() {
+		// all bricks are destroyed
+		if (Brick.breakable_Count <= 0) {
+			LoadNextLevel();
+		}
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// The script governs music being played in the background
+// The game music is played throughout the game regardless level
+
+public class MusicPlayer : MonoBehaviour {
+
+	// static class variable
+	// determine if music is currently playing
+	static MusicPlayer instance = null;
+
+	void Awake () {
+		Debug.Log ("Music player Awakes " + GetInstanceID());
+		// the instance check logic is in Awake method
+		// it prevents brief double music glitch at Start scene
+		if (instance != null) {
+			// another music instance is already playing
+			// destruct the newly created instance, current gameObject
+			Destroy (gameObject);
+			Debug.Log ("Duplicate music player self-destructs " + GetInstanceID());
+		}
+		else {
+			// no other music player instance
+			// claim this instance
+			instance = this;
+			// enable the music to play throughout the game
+			// gameObject is the Music Player object
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+		
+	// Use this for initialisation
+	void Start () {
+		Debug.Log ("Music player Starts " + GetInstanceID());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
