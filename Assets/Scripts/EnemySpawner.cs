8bc3307023cddc9583cd6990a1e8c32789db3c74
Terry Song <jtsong8@gmail.com>
2016-01-24 00:47:34 -0500

    modifying enemy formation

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class EnemySpawner : MonoBehaviour {
     public GameObject enemy_prefab;
+    public float width;
+    public float height;
+
+    private float x_min;
+    private float x_max;
 
 	// Use this for initialization
 	void Start () {
-        // create an enemy with Instantiate
-        GameObject enemy = Instantiate(enemy_prefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-        enemy.transform.parent = transform;
+        width = 10f;
+        height = 5f;
+
+        // private variables
+        x_min = -2f;
+        x_max = 2f;
+
+        // spawn an enemy with the parent formation object
+        foreach(Transform child in transform)
+        {
+            // create an enemy with Instantiate
+            GameObject enemy = Instantiate(enemy_prefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
 	}
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0f));
+    }
 	
 	// Update is called once per frame
 	void Update () {
