fbb650f403a5bab22fef9c4523b1b715228c7ef7
Terry Song <jtsong8@gmail.com>
2016-01-23 21:20:51 -0500

    added enemy position gizmo in enemy formation

---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+    // an on draw Gizmo script to show the position in editor
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, 1);
+    }
+}
